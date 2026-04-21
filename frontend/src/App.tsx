0c20737062b7873eb2574355f93c4860c76aa3b6
Renato Martins <108689676+renatomart15@users.noreply.github.com>
2026-04-21 19:22:56 -0300

    feat: Component de Input para criar Tasks
    Esse componente é o responsável pode permitir com que o usuário crie tasks, além de que também foi feita uma mudança em onde o hook useTasks é chamado.
    
    Ele passou a ser chamado no App.tsx pq agora mais de um componente precisa das funciodalidadas do hook, e também, eu preciso que o estado "tasks", presente no useTasks, fique em um só lugar para que não haja conflito de estados durante o uso da aplicação.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,24 @@
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import TaskInput from "./components/TaskInput";
 import TaskList from "./components/TaskList";
+import { useTasks } from "./hooks/useTasks";
 
 function App() {
+  const { tasks, createTask, deleteTask, updateTask, changeStatus } =
+    useTasks();
   return (
     <main className="bg-[#f3f4f6] dark:bg-[#101828] min-h-screen flex transition-colors duration-300">
       <Sidebar />
       <div className="flex-1">
         <Header />
-        <TaskList />
+        <TaskInput createTask={createTask} />
+        <TaskList
+          tasks={tasks}
+          deleteTask={deleteTask}
+          updateTask={updateTask}
+          changeStatus={changeStatus}
+        />
       </div>
     </main>
   );
---
+++ frontend/src/components/TaskInput.tsx
@@ -0,0 +1,35 @@
+import { Plus } from "lucide-react";
+import { useState } from "react";
+
+type TaskInputProps = {
+  createTask: (title: string) => void;
+};
+
+const TaskInput = ({ createTask }: TaskInputProps) => {
+  const [title, setTitle] = useState("");
+  const handleSubmit = () => {
+    if (title === "") return;
+    createTask(title);
+    setTitle("");
+  };
+  return (
+    <div className="flex gap-5 mx-auto mb-8 mt-10 w-110">
+      <input
+        type="text"
+        placeholder="Digite sua task aqui..."
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        className="bg-white py-2 px-4 rounded-lg shadow-md flex-1 focus:outline-none placeholder:text-lg text-lg dark:text-white dark:bg-[#364153]"
+      />
+      <button
+        className="flex gap-1 text-lg items-center text-white bg-black py-1 px-5 rounded-lg shadow-md active:scale-98 transition duration-300 cursor-pointer dark:bg-[#4a5565]"
+        type="submit"
+        onClick={() => handleSubmit()}
+      >
+        <Plus size={18} /> Add
+      </button>
+    </div>
+  );
+};
+
+export default TaskInput;
