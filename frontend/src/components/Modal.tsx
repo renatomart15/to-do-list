f50669b44da8093cbc8ec9fb04d7df631729ccf9
Renato Martins <108689676+renatomart15@users.noreply.github.com>
2026-04-21 19:24:11 -0300

    style: pequena alteração no estilo dos botões do modal

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -12,8 +12,18 @@ const Modal = ({ isOpen, onConfirm, onCancel, message }: ModalProps) => {
       <div className="bg-white dark:bg-[#1e2939] dark:text-white  p-6 rounded-lg text-lg font-semibold select-none">
         <p className="pb-10">{message}</p>
         <div className="flex justify-between">
-          <button onClick={onCancel} className="py-2 px-5 rounded-lg border cursor-pointer">Cancelar</button>
-          <button onClick={onConfirm} className="py-2 px-5 rounded-lg bg-red-500 text-white cursor-pointer">Confirmar</button>
+          <button
+            onClick={onCancel}
+            className="py-2 px-5 rounded-lg border cursor-pointer active:scale-98 transition duration-300"
+          >
+            Cancelar
+          </button>
+          <button
+            onClick={onConfirm}
+            className="py-2 px-5 rounded-lg bg-red-500 text-white cursor-pointer active:scale-98 transition duration-300"
+          >
+            Confirmar
+          </button>
         </div>
       </div>
     </div>
