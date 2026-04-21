136695bea9a662858de26d0a5ac8d1c3dff6c2ed
Renato Martins <108689676+renatomart15@users.noreply.github.com>
2026-04-21 19:23:49 -0300

    feat: criação dos props para o componente TaskList receber as funções que precisa

--- frontend/src/components/TaskList.tsx
+++ frontend/src/components/TaskList.tsx
@@ -1,8 +1,20 @@
-import { useTasks } from "../hooks/useTasks";
 import TaskItem from "./TaskItem";
 
-const TaskList = () => {
-  const { tasks, changeStatus, deleteTask, updateTask } = useTasks();
+import type { Task } from "../hooks/useTasks";
+
+type TaskListProps = {
+  tasks: Task[];
+  deleteTask: (id: number) => void;
+  updateTask: (id: number, title: string) => void;
+  changeStatus: (id: number) => void;
+};
+
+const TaskList = ({
+  tasks,
+  changeStatus,
+  deleteTask,
+  updateTask,
+}: TaskListProps) => {
   return (
     <div className="flex flex-col items-center gap-4 my-6 text-gray-600 dark:text-gray-500">
       {tasks.length > 0 ? (
@@ -13,11 +25,17 @@ const TaskList = () => {
               <span className="cursor-pointer">Active</span>|
               <span className="cursor-pointer">Completed</span>
             </div>
-            <p>2 tasks left</p>
+            <p>{tasks.length} tasks left</p>
           </div>
           <ul className="flex flex-col gap-5">
             {tasks.map((task) => (
-              <TaskItem key={task.id} task={task} changeStatus={changeStatus} deleteTask={deleteTask} updateTask={updateTask} />
+              <TaskItem
+                key={task.id}
+                task={task}
+                changeStatus={changeStatus}
+                deleteTask={deleteTask}
+                updateTask={updateTask}
+              />
             ))}
           </ul>
         </>
