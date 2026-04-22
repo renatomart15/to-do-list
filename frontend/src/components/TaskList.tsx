3012493d621ec40e8202fddb532f9ce88fe06b2a
Renato Martins <108689676+renatomart15@users.noreply.github.com>
2026-04-22 20:33:11 -0300

    feat: Adicionei a filtragem de tasks (All, Active, Completed)

--- frontend/src/components/TaskList.tsx
+++ frontend/src/components/TaskList.tsx
@@ -1,4 +1,5 @@
 import TaskItem from "./TaskItem";
+import { useState } from "react";
 
 import type { Task } from "../hooks/useTasks";
 
@@ -15,20 +16,43 @@ const TaskList = ({
   deleteTask,
   updateTask,
 }: TaskListProps) => {
+  const [filter, setFilter] = useState("all");
+  const filteredTasks = tasks.filter((task) => {
+    if (filter === "active") return !task.done;
+    if (filter === "completed") return task.done;
+    return true; // "all"
+  });
   return (
     <div className="flex flex-col items-center gap-4 my-6 text-gray-600 dark:text-gray-500">
       {tasks.length > 0 ? (
         <>
           <div className="flex justify-between w-108 text-sm">
-            <div className="flex gap-1 items-center">
-              <span className="cursor-pointer">All</span>|
-              <span className="cursor-pointer">Active</span>|
-              <span className="cursor-pointer">Completed</span>
+            <div className="flex gap-1 items-center select-none">
+              <span
+                onClick={() => setFilter("all")}
+                className={`cursor-pointer ${filter === "all" ? "text-black font-semibold dark:text-white" : ""}`}
+              >
+                All
+              </span>
+              |
+              <span
+                onClick={() => setFilter("active")}
+                className={`cursor-pointer ${filter === "active" ? "text-black font-semibold dark:text-white" : ""}`}
+              >
+                Active
+              </span>
+              |
+              <span
+                onClick={() => setFilter("completed")}
+                className={`cursor-pointer ${filter === "completed" ? "text-black font-semibold dark:text-white" : ""}`}
+              >
+                Completed
+              </span>
             </div>
-            <p>{tasks.length} tasks left</p>
+            <p>{filteredTasks.length} tasks left</p>
           </div>
           <ul className="flex flex-col gap-5">
-            {tasks.map((task) => (
+            {filteredTasks.map((task) => (
               <TaskItem
                 key={task.id}
                 task={task}
