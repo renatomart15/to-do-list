85c0dcd89ace95069f9fcd0ef3d40043fac085fc
Renato Martins <108689676+renatomart15@users.noreply.github.com>
2026-04-23 20:53:51 -0300

    style: modificação do tema padrão para o dark

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
         onClick={toggleTheme}
         className="text-black dark:text-white cursor-pointer pt-5 pr-5"
       >
-        {theme === "light" ? <Sun size={30} className="hover:text-yellow-500 transition" /> : <Moon size={30} className="hover:text-blue-500 transition" />}
+        {theme === "dark" ? <Sun size={30} className="hover:text-yellow-500 transition" /> : <Moon size={30} className="hover:text-blue-500 transition" />}
       </button>
       <h1 className="text-center text-black dark:text-white text-3xl font-semibold">My Tasks</h1>
     </header>
